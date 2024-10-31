788b966c9339987dd4a0281dbda4236bc2e70256
kauequerino <kauequerino77@gmail.com>
2024-10-31 12:29:47 -0300

    Criação tela, Logo, Imagens
    Criado a tela sobre, a logo foi adicionada e algumas imagens extras

--- src/app/shared/cabecalho/cabecalho.component.ts
+++ src/app/shared/cabecalho/cabecalho.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, NgModule, OnInit, Output} from '@angular/core';
 import {IonicModule} from "@ionic/angular";
+import { RouterModule } from '@angular/router';
 
 interface BarraPesquisa {
   mostrar: boolean;
